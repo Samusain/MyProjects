0e433dc13b742d37b4c7ffcf900f67b51cd3751b
Samusain <samzy334@gmail.com>
2024-08-17 13:31:05 +0100

    Responsive design applied

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,40 +1,55 @@
 import React from 'react'
 import "./Header.css"
 import { Link } from "react-router-dom"
+import Burger from '../../images/icon-hamburger.svg'
+import Close from '../../images/icon-close.svg'
 
 const Header = () => {
-//     const handleClick = () => {
-//         style.display = 'block';
-//     }
+    const closeBtn = document.querySelector('.closebtn');
+    const burgerMenu = document.querySelector('.burger');
+
+    const showSidebar = () => {
+        const sidebar = document.querySelector('.sidebar');
+        closeBtn.style.display = 'block';
+        burgerMenu.style.display = 'none';
+        sidebar.style.display = 'flex';
+    }
+
+    const hideSidebar = () => {
+        const sidebar = document.querySelector('.sidebar');
+        closeBtn.style.display = 'none';
+        burgerMenu.style.display = 'block';
+        sidebar.style.display = 'none';
+    }
 
   return (
     <header>
         <nav className='menubar'>
-                <p>Blogr</p>
-            <div className='name'>
+            <p>Blogr</p>
+            <div className='sidebar'>
                 <ul>
-                    <li><a href='#'>Product</a>
+                    <li><a href='/'>Product</a>
                         <ul>
-                            <li><a href='#'>Overview</a></li>
-                            <li><a href='#'>Pricing</a></li>
-                            <li><a href='#'>Marketplace</a></li>
-                            <li><a href='#'>Features</a></li>
-                            <li><a href='#'>Integrations</a></li>
+                            <li><a href='/'>Overview</a></li>
+                            <li><a href='/'>Pricing</a></li>
+                            <li><a href='/'>Marketplace</a></li>
+                            <li><a href='/'>Features</a></li>
+                            <li><a href='/'>Integrations</a></li>
                         </ul>
                     </li>
-                    <li><a href='#'>Company</a>
+                    <li><a href='/'>Company</a>
                         <ul>
-                            <li><a href="#">About</a></li>
-                            <li><a href="#">Team</a></li>
-                            <li><a href="#">Blog</a></li>
-                            <li><a href="#">Careers</a></li>
+                            <li><a href="/">About</a></li>
+                            <li><a href="/">Team</a></li>
+                            <li><a href="/">Blog</a></li>
+                            <li><a href="/">Careers</a></li>
                         </ul>
                     </li>
-                    <li><a href='#'>Connect</a>
+                    <li><a href='/'>Connect</a>
                         <ul>
-                            <li><a href="#">Contact</a></li>
-                            <li><a href="#">Newsletter</a></li>
-                            <li><a href="#">LinkedIn</a></li>
+                            <li><a href="/">Contact</a></li>
+                            <li><a href="/">Newsletter</a></li>
+                            <li><a href="/">LinkedIn</a></li>
                         </ul>
                     </li>
                 </ul>
@@ -43,46 +58,45 @@ const Header = () => {
                 <Link to="details" className='signup'>Sign Up</Link>
             </div>
             </div>
-            <input type="checkbox" id="check"/>
-            <label for="check" className="checkbtn">
-                <span className="material-symbols-outlined">menu</span>
-            </label>
-
-
-            {/* <div className="dropdown">
-            <div className='name'>
+            
+            <div className='menu'>
                 <ul>
-                    <li><a href='#'>Product</a>
+                    <li><a href='/'>Product</a>
                         <ul>
-                            <li><a href='#'>Overview</a></li>
-                            <li><a href='#'>Pricing</a></li>
-                            <li><a href='#'>Marketplace</a></li>
-                            <li><a href='#'>Features</a></li>
-                            <li><a href='#'>Integrations</a></li>
+                            <li><a href='/'>Overview</a></li>
+                            <li><a href='/'>Pricing</a></li>
+                            <li><a href='/'>Marketplace</a></li>
+                            <li><a href='/'>Features</a></li>
+                            <li><a href='/'>Integrations</a></li>
                         </ul>
                     </li>
-                    <li><a href='#'>Company</a>
+                    <li><a href='/'>Company</a>
                         <ul>
-                            <li><a href="#">About</a></li>
-                            <li><a href="#">Team</a></li>
-                            <li><a href="#">Blog</a></li>
-                            <li><a href="#">Careers</a></li>
+                            <li><a href="/">About</a></li>
+                            <li><a href="/">Team</a></li>
+                            <li><a href="/">Blog</a></li>
+                            <li><a href="/">Careers</a></li>
                         </ul>
                     </li>
-                    <li><a href='#'>Connect</a>
+                    <li><a href='/'>Connect</a>
                         <ul>
-                            <li><a href="#">Contact</a></li>
-                            <li><a href="#">Newsletter</a></li>
-                            <li><a href="#">LinkedIn</a></li>
+                            <li><a href="/">Contact</a></li>
+                            <li><a href="/">Newsletter</a></li>
+                            <li><a href="/">LinkedIn</a></li>
                         </ul>
                     </li>
                 </ul>
             <div className="loggins">
-                <Link to="/details" className='login'>Login</Link>
-                <Link to="/details" className='signup'>Sign Up</Link>
+                <Link to="details" className='login'>Login</Link>
+                <Link to="details" className='signup'>Sign Up</Link>
             </div>
             </div>
-            </div> */}
+            
+            {/* <div className='burger-menu'> */}
+                <img src={ Close } alt="icon-close" className='closebtn' onClick={hideSidebar}/>
+                <img src={ Burger } alt="icon-hamburger" className='burger' onClick={showSidebar}/>
+            {/* </div> */}
+           
         </nav>
     </header>
   )
--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import "./Main.css"
 import Img1 from '../../images/illustration-phones.svg'
 import Img2 from '../../images/illustration-laptop-desktop.svg'
+import Img3 from '../../images/illustration-editor-desktop.svg'
 import { Link } from 'react-router-dom'
 
 
@@ -22,35 +23,36 @@ const Main = () => {
   <section className="future-dsn">
     <div className="future-box">
       <h2>Designed for the future</h2>
+      <img src={ Img3 } alt="illustration-editor" className="editor-img"/>
       <article className="editor">
         <h3>Introducing an extensible editor</h3>
-        <p>Blogr features an exceedingly intuitive interface which lets you focus <br/> on one thing: creating content. The editor supports management of <br/> multiple blogs and allows easy manipulation of embeds such as images,<br/> videos, and Markdown. Extensibility with plugins and themes provide <br/> easy ways to add functionality or change the looks of a blog.</p>
+        <p>Blogr features an exceedingly intuitive interface which lets you focus  on one thing: creating content. The editor supports management of multiple blogs and allows easy manipulation of embeds such as images, videos, and Markdown. Extensibility with plugins and themes provideways to add functionality or change the looks of a blog.</p>
   
         <h3>Robust content management</h3>
-        <p>Flexible content management enables users to easily move through <br/> posts. Increase the usability of your blog by adding customized <br/> categories, sections, format, or flow. With this functionality, you're in <br/> full control.</p>
+        <p>Flexible content management enables users to easily move through posts. Increase the usability of your blog by adding customized categories, sections, format, or flow. With this functionality, you're in full control.</p>
       </article>
     </div>
   </section>
 
-  <section class="art">
-    <div class="art-box">
-      <img src={ Img1 } alt="illustration-phones" class="phones"/>
-      <article class="state-art">
+  <section className="art">
+    <div className="art-box">
+      <img src={ Img1 } alt="illustration-phones" className="phones"/>
+      <article className="state-art">
         <h2>State of the Art Infrastructure</h2>
-        <p>With reliability and speed in mind, worldwide data centers provide the <br/> backbone for ultra-fast connectivity. This ensures your site will load <br/> instantly, no matter where your readers are, keeping your site <br/> competitive.</p>
+        <p>With reliability and speed in mind, worldwide data centers provide the backbone for ultra-fast connectivity. This ensures your site will load instantly, no matter where your readers are, keeping your site competitive.</p>
       </article>
     </div>
   </section>
 
-  <section class="free">
-    <div class="free-box">
-      <img src={ Img2 } alt="illustration-laptop-desktop" class="laptop"/>
-      <article class="editor">
+  <section className="free">
+    <div className="free-box">
+      <img src={ Img2 } alt="illustration-laptop-desktop" className="laptop"/>
+      <article className="editor">
         <h3>Free, open, simple</h3>
-        <p>Blogr is a free and open source application backed by a large <br/> community of helpful developers. It supports features such as code <br/> syntax highlighting, RSS feeds, social media integration, third-party <br/>commenting tools, and works seamlessly with Google Analytics. The <br/> architecture is clean and is relatively easy to learn.</p>
+        <p>Blogr is a free and open source application backed by a large community of helpful developers. It supports features such as code syntax highlighting, RSS feeds, social media integration, third-party <br/>commenting tools, and works seamlessly with Google Analytics. The architecture is clean and is relatively easy to learn.</p>
 
         <h3>Powerful tooling</h3>
-        <p>Batteries included. We built a simple and straightforward CLI tool that <br/> makes customization and deployment a breeze, but capable of <br/> producing even the most complicated sites.</p>
+        <p>Batteries included. We built a simple and straightforward CLI tool that makes customization and deployment a breeze, but capable of producing even the most complicated sites.</p>
       </article>
     </div>
   </section>
