4c44504f0bc8585bb29f9e8510fce06d2fef770f
Samusain <samzy334@gmail.com>
2024-08-20 16:11:34 +0100

    Menubar Completed

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,105 +1,108 @@
-import React from 'react'
-import "./Header.css"
-import { Link } from "react-router-dom"
-import Burger from '../../images/icon-hamburger.svg'
-import Close from '../../images/icon-close.svg'
+import React from "react";
+import "./Header.css";
+import { Link } from "react-router-dom";
+import { FiMenu } from "react-icons/fi";
+import { IoCloseSharp } from "react-icons/io5";
 
 const Header = () => {
-    const closeBtn = document.querySelector('.closebtn');
-    const burgerMenu = document.querySelector('.burger');
 
     const showSidebar = () => {
-        const sidebar = document.querySelector('.sidebar');
-        closeBtn.style.display = 'block';
-        burgerMenu.style.display = 'none';
-        sidebar.style.display = 'flex';
-    }
+        const closeBtn = document.getElementById("Closebtn");
+        const burgerMenu = document.getElementById("Burger");
+        const modal = document.getElementById('Modal');
+        const dropMenu = document.getElementById('Menu');
 
+        dropMenu.classList.add('dropmenu');
+        closeBtn.classList.add('show');
+        burgerMenu.classList.add("hide");
+        modal.classList.add('open-modal');
+    };
+    
     const hideSidebar = () => {
-        const sidebar = document.querySelector('.sidebar');
-        closeBtn.style.display = 'none';
-        burgerMenu.style.display = 'block';
-        sidebar.style.display = 'none';
-    }
+        const closeBtn = document.getElementById("Closebtn");
+        const burgerMenu = document.getElementById("Burger");
+        const modal = document.getElementById('Modal');
+        const dropMenu = document.getElementById('Menu');
 
-  return (
-    <header>
-        <nav className='menubar'>
-            <p>Blogr</p>
-            <div className='sidebar'>
-                <ul>
-                    <li><a href='/'>Product</a>
-                        <ul>
-                            <li><a href='/'>Overview</a></li>
-                            <li><a href='/'>Pricing</a></li>
-                            <li><a href='/'>Marketplace</a></li>
-                            <li><a href='/'>Features</a></li>
-                            <li><a href='/'>Integrations</a></li>
-                        </ul>
-                    </li>
-                    <li><a href='/'>Company</a>
-                        <ul>
-                            <li><a href="/">About</a></li>
-                            <li><a href="/">Team</a></li>
-                            <li><a href="/">Blog</a></li>
-                            <li><a href="/">Careers</a></li>
-                        </ul>
-                    </li>
-                    <li><a href='/'>Connect</a>
-                        <ul>
-                            <li><a href="/">Contact</a></li>
-                            <li><a href="/">Newsletter</a></li>
-                            <li><a href="/">LinkedIn</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            <div className="loggins">
-                <Link to="details" className='login'>Login</Link>
-                <Link to="details" className='signup'>Sign Up</Link>
-            </div>
-            </div>
-            
-            <div className='menu'>
-                <ul>
-                    <li><a href='/'>Product</a>
-                        <ul>
-                            <li><a href='/'>Overview</a></li>
-                            <li><a href='/'>Pricing</a></li>
-                            <li><a href='/'>Marketplace</a></li>
-                            <li><a href='/'>Features</a></li>
-                            <li><a href='/'>Integrations</a></li>
-                        </ul>
-                    </li>
-                    <li><a href='/'>Company</a>
-                        <ul>
-                            <li><a href="/">About</a></li>
-                            <li><a href="/">Team</a></li>
-                            <li><a href="/">Blog</a></li>
-                            <li><a href="/">Careers</a></li>
-                        </ul>
-                    </li>
-                    <li><a href='/'>Connect</a>
-                        <ul>
-                            <li><a href="/">Contact</a></li>
-                            <li><a href="/">Newsletter</a></li>
-                            <li><a href="/">LinkedIn</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            <div className="loggins">
-                <Link to="details" className='login'>Login</Link>
-                <Link to="details" className='signup'>Sign Up</Link>
-            </div>
-            </div>
-            
-            {/* <div className='burger-menu'> */}
-                <img src={ Close } alt="icon-close" className='closebtn' onClick={hideSidebar}/>
-                <img src={ Burger } alt="icon-hamburger" className='burger' onClick={showSidebar}/>
-            {/* </div> */}
-           
-        </nav>
-    </header>
-  )
-}
+        dropMenu.classList.remove('dropmenu');
+        closeBtn.classList.remove('show');
+        burgerMenu.classList.remove("hide");
+        modal.classList.remove('open-modal');
+    };
 
-export default Header
+
+    return (
+        <header>
+            <nav className="menubar">
+                <h1>Blogr</h1>
+                <div className="menu" id="Menu">
+                    <ul>
+                        <li>
+                            <a to="details">Product</a>
+                            <ul>
+                                <li>
+                                    <a to="details">Overview</a>
+                                </li>
+                                <li>
+                                    <a to="details">Pricing</a>
+                                </li>
+                                <li>
+                                    <a to="details">Marketplace</a>
+                                </li>
+                                <li>
+                                    <a to="details">Features</a>
+                                </li>
+                                <li>
+                                    <a to="details">Integrations</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a to="details" >Company</a>
+                            <ul>
+                                <li>
+                                    <a to="details">About</a>
+                                </li>
+                                <li>
+                                    <a to="details">Team</a>
+                                </li>
+                                <li>
+                                    <a to="details">Blog</a>
+                                </li>
+                                <li>
+                                    <a to="details">Careers</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a to="details">Connect</a>
+                            <ul>
+                                <li>
+                                    <a to="details">Contact</a>
+                                </li>
+                                <li>
+                                    <a to="details">Newsletter</a>
+                                </li>
+                                <li>
+                                    <a to="details">LinkedIn</a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <div className="loggins">
+                        <Link to="details" className="login">Login</Link>
+                        <Link to="details" className="signup">Sign Up</Link>
+                    </div>
+                </div>
+                <div className='burger-menu'>
+                    <FiMenu id="Burger" className="burger" onClick={showSidebar}/>
+                    <IoCloseSharp id="Closebtn" className="closebtn" onClick={hideSidebar}/>
+                </div>
+                <span className="modal" id='Modal' onClick={hideSidebar}></span>
+
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
